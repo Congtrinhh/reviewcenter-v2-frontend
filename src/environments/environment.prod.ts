15f78594b49e1ba8c764ab291e2f077fcfd3f3e4
Congtrinhh <trinhquycong@gmail.com>
2022-03-03 14:36:15 +0700

    changed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiBaseUrl: 'https://reviewcenter-backend.herokuapp.com/',
-  oAuth2RedirectUrl: '?redirect_uri=https://reviewcenter.herokuapp.com',
+  oAuth2RedirectUrl: '?redirect_uri=https://reviewcenter.herokuapp.com/login',
 };
