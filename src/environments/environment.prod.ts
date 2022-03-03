2e82c8e8b298644778d6be013a63dd2c7a24927b
Congtrinhh <trinhquycong@gmail.com>
2022-03-03 13:55:10 +0700

    change variables in enviroment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'http://www.ahihi.com/',
-  oAuth2RedirectUrl: '?redirect_uri=http://localhost:8081/login',
+  apiBaseUrl: 'https://reviewcenter-backend.herokuapp.com/',
+  oAuth2RedirectUrl: '?redirect_uri=https://reviewcenter.herokuapp.com',
 };
